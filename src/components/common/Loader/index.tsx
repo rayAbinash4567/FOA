6cf45adbc61abe007b265ef10998526d5a01fd31
Pratik Upreti <pratikupreti1@hotmail.com>
2024-05-06 19:33:43 -0400

    fix: minor bugs in UI

--- src/components/common/Loader/index.tsx
+++ src/components/common/Loader/index.tsx
@@ -1,6 +1,7 @@
 const Loader = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-white dark:bg-black">
+      <h2 className="text-xl font-satoshi">Please wait for a while....</h2>
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
